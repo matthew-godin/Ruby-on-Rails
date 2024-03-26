abde0307a01007afb381ca4b8a953448c548e586
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-03-26 05:09:43 -0400

    wiki posts

--- wiki_app/db/schema.rb
+++ wiki_app/db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_26_082632) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_26_090329) do
   create_table "wiki_posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
